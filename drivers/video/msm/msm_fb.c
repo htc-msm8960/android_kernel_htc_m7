494fdadc4ba52f10714368a226bb86fbb99ca10d
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2013-04-25 17:57:46 +0530

    msm_fb: Set backlight to zero during adb shell stop scenario
    During framework reboot (adb shell stop/start), the backlight
    value might be non-zero after adb shell stop. In such cases,
    during adb shell start, we see momentary garbled screen due to
    panel-init pattern since backlight is ON. Hence, set backlight
    to zero during adb shell stop. Also, store the latest backlight
    value before adb shell stop so that backlight can be set soon
    after first frame update after adb shell start.
    
    CRs-fixed: 479316
    Change-Id: I2a98c2599f5f3fadf516da2df06556f52fe50a2e
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -1321,8 +1321,9 @@ static int msm_fb_blank_sub(int blank_mode, struct fb_info *info,
 			mfd->op_enable = FALSE;
 			curr_pwr_state = mfd->panel_power_on;
 			down(&mfd->sem);
- 			mfd->panel_power_on = FALSE;
-			bl_updated = 0;
+			mfd->panel_power_on = FALSE;
+			if (mfd->fbi->node == 0)
+				bl_updated = 0;
 			up(&mfd->sem);
 			cancel_delayed_work_sync(&mfd->backlight_worker);
                         
@@ -2151,7 +2152,7 @@ static void msm_fb_free_base_pipe(struct msm_fb_data_type *mfd)
 static int msm_fb_release(struct fb_info *info, int user)
 {
 	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)info->par;
-	int ret = 0;
+	int ret = 0, bl_level = 0;
 
 	if (!mfd->ref_cnt) {
 		MSM_FB_INFO("msm_fb_release: try to close unopened fb %d!\n",
@@ -2163,6 +2164,13 @@ static int msm_fb_release(struct fb_info *info, int user)
 
 	if (!mfd->ref_cnt) {
 		if (mfd->op_enable) {
+			if (info->node == 0) {
+				down(&mfd->sem);
+				bl_level = mfd->bl_level;
+				msm_fb_set_backlight(mfd, 0);
+				unset_bl_level = bl_level;
+				up(&mfd->sem);
+			}
 			ret = msm_fb_blank_sub(FB_BLANK_POWERDOWN, info,
 							mfd->op_enable);
 			if (ret != 0) {
@@ -2504,7 +2512,7 @@ static void msm_fb_commit_wq_handler(struct work_struct *work)
 	complete_all(&mfd->commit_comp);
 	mutex_unlock(&mfd->sync_mutex);
 
-	if (unset_bl_level && !bl_updated)
+	if (!bl_updated)
 		schedule_delayed_work(&mfd->backlight_worker,
 					backlight_duration);
 }
